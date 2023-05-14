6d244b4e51d646c155f8ee4605952fd23cb4c196
Marcus Hellams <mhellams@hotmail.com>
2023-05-14 19:48:55 -0400

    set headers

--- app/graphql/route.ts
+++ app/graphql/route.ts
@@ -1,13 +1,14 @@
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { ApolloServer } from '@apollo/server';
 import { gql } from 'graphql-request';
+import { NextRequest, NextResponse } from 'next/server';
+import { NextApiRequest } from 'next';
 
 const resolvers = {
   Query: {
     hello: () => 'world',
   },
 };
-
 
 const typeDefs = gql`
   type Query {
@@ -23,9 +24,29 @@ const server = new ApolloServer({
 const handler = startServerAndCreateNextHandler(server);
 
 export async function GET(request: Request) {
-  return handler(request);
+  const response = await handler(request);
+
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  response.headers.set(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization'
+  );
+  response.headers.set('Access-Control-Allow-Credentials', 'true');
+
+  return response;
 }
 
 export async function POST(request: Request) {
-  return handler(request);
+  const response = await handler(request);
+
+  response.headers.set('Access-Control-Allow-Origin', '*');
+  response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  response.headers.set(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization'
+  );
+  response.headers.set('Access-Control-Allow-Credentials', 'true');
+
+  return response;
 }
