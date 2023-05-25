24cb9dd40f75b6d5f31a4d7509a8568c451db30e
MarcusTHellams <mhellams@hotmail.com>
2023-05-25 19:02:27 -0400

    update graphql url

--- graphql/client.ts
+++ graphql/client.ts
@@ -1,3 +1,3 @@
 import { GraphQLClient } from 'graphql-request';
 
-export const client = new GraphQLClient(`${process.env.NEXT_PUBLIC_API_URL}/graphql`);
+export const client = new GraphQLClient(process.env.NEXT_PUBLIC_API_URL as string);
